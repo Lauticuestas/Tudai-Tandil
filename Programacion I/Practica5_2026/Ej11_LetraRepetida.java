fb8352e360fa01cd87c0e876f97bffc4a5b69173
Lauticuestas <lautycuestas23@gmail.com>
2026-05-09 17:12:01 -0300

    add Ej11

---
+++ Programacion I/Practica5_2026/Ej11_LetraRepetida.java
@@ -0,0 +1,44 @@
+package Practica5_2026;
+
+/*
+11. Dado un arreglo de caracteres, determinar si en el arreglo 
+existe al menos una letra (verificar que es letra) repetida. 
+a. En posiciones contiguas.  
+b. En cualquier posición. 
+*/
+
+public class Ej11_LetraRepetida {
+    public static void main(String[] args) {
+        char[] arr = { 'a', 'b', 'c', 'c', 'd', 'c', 'f', 'c' };
+        boolean juntas = buscarRepetidasJuntas(arr);
+        boolean repetidas = buscarRepetidas(arr);
+        System.out.println("-- Resultado --");
+        if (juntas) {
+            System.out.println("En este arr hay letras repetidas contiguas.");
+        }
+        if (repetidas) {
+            System.out.println("En este arr hay letras repetidas.");
+        }
+    }
+
+    public static boolean buscarRepetidasJuntas(char[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean buscarRepetidas(char[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int letra = arr[i];
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] == letra && j != i) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
