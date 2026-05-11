b0fbc5b12d63e895fd1d3a0260b0c5eb3b9cc56a
Lauticuestas <lautycuestas23@gmail.com>
2026-05-11 11:22:08 -0300

    add Ej17

--- Programacion I/Practica5_2026/Ej17_CorrimientoDerecha.java
+++ Programacion I/Practica5_2026/Ej17_CorrimientoDerecha.java
@@ -1,21 +1,28 @@
 package Practica5_2026;
 
+/*
+17. Implementar un método que realice un corrimiento a derecha 
+en un arreglo ordenado de tamaño 10 a partir de una posición.
+*/
+
 public class Ej17_CorrimientoDerecha {
     public static void main(String[] args) {
-        int[] arr={2,3,5,7,8,9,10,12,14,17};
-        int pos=7;
-        corrimientoD(arr,pos);
-        System.out.println("-- El arreglo con el corrimiento desde la pos: "+pos+" --");
+        int[] arr = { 2, 3, 5, 7, 8, 9, 10, 12, 14, 17 };
+        int pos = 7;
+        corrimientoD(arr, pos);
+        System.out.println("-- El arreglo con el corrimiento desde la pos: " + pos + " --");
         mostrarArr(arr);
     }
-    public static void corrimientoD(int[] arr,int pos){
-        for(int i=arr.length-1;i>pos;i--){
-            arr[i]=arr[i-1];
+
+    public static void corrimientoD(int[] arr, int pos) {
+        for (int i = arr.length - 1; i > pos; i--) {
+            arr[i] = arr[i - 1];
         }
     }
-    public static void mostrarArr(int[] arr){
-        for(int n:arr){
-            System.out.print("["+n+"]");
+
+    public static void mostrarArr(int[] arr) {
+        for (int n : arr) {
+            System.out.print("[" + n + "]");
         }
         System.out.println();
     }
